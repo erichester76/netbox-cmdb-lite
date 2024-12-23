254793e737e513f3a064b3b4d9894c9af7318ab9
Eric Hester <eric.hester@umbrella.tech>
2024-12-22 20:17:49 -0500

    fixed view import

--- netbox_cmdb_lite/api/__init__.py
+++ netbox_cmdb_lite/api/__init__.py
@@ -1,1 +1,2 @@
+from netbox.views import NetBoxListView
 # This file is intentionally left blank.
--- netbox_cmdb_lite/views.py
+++ netbox_cmdb_lite/views.py
@@ -1,68 +1,68 @@
-from netbox.views import NetBoxListView, NetBoxEditView, NetBoxDeleteView
+from netbox.views import generic
 from . import models
 from . import tables
 from . import forms
 
 ###################################
-# Adjusted NetBox Plugin Views
+# Adjusted generic.Object Plugin Views
 ###################################
 
 # ObjectType Views
-class ObjectTypeListView(NetBoxListView):
+class ObjectTypeListView(generic.ObjectListView):
     queryset = models.ObjectType.objects.all()
     table = tables.ObjectTypeTable
     template_name = "object_types/object_type_list.html"
 
-class ObjectTypeEditView(NetBoxEditView):
+class ObjectTypeEditView(generic.ObjectEditView):
     queryset = models.ObjectType.objects.all()
     model_form = forms.ObjectTypeForm
     template_name = "object_types/object_type_edit.html"
 
-class ObjectTypeDeleteView(NetBoxDeleteView):
+class ObjectTypeDeleteView(generic.ObjectDeleteView):
     queryset = models.ObjectType.objects.all()
     template_name = "object_types/object_type_delete.html"
 
 # RelationshipType Views
-class RelationshipTypeListView(NetBoxListView):
+class RelationshipTypeListView(generic.ObjectListView):
     queryset = models.RelationshipType.objects.all()
     table = tables.RelationshipTypeTable
     template_name = "relationships/relationship_type_list.html"
 
-class RelationshipTypeEditView(NetBoxEditView):
+class RelationshipTypeEditView(generic.ObjectEditView):
     queryset = models.RelationshipType.objects.all()
     model_form = forms.RelationshipTypeForm
     template_name = "relationships/relationship_type_edit.html"
 
-class RelationshipTypeDeleteView(NetBoxDeleteView):
+class RelationshipTypeDeleteView(generic.ObjectDeleteView):
     queryset = models.RelationshipType.objects.all()
     template_name = "relationships/relationship_type_delete.html"
 
 # GenericObject Views
-class GenericObjectListView(NetBoxListView):
+class GenericObjectListView(generic.ObjectListView):
     queryset = models.GenericObject.objects.all()
     table = tables.GenericObjectTable
     template_name = "generic_objects/generic_object_list.html"
 
-class GenericObjectEditView(NetBoxEditView):
+class GenericObjectEditView(generic.ObjectEditView):
     queryset = models.GenericObject.objects.all()
     model_form = forms.GenericObjectForm
     template_name = "generic_objects/generic_object_edit.html"
 
-class GenericObjectDeleteView(NetBoxDeleteView):
+class GenericObjectDeleteView(generic.ObjectDeleteView):
     queryset = models.GenericObject.objects.all()
     template_name = "generic_objects/generic_object_delete.html"
 
 # GenericRelationship Views
-class GenericRelationshipListView(NetBoxListView):
+class GenericRelationshipListView(generic.ObjectListView):
     queryset = models.GenericRelationship.objects.all()
     table = tables.GenericRelationshipTable
     template_name = "relationships/generic_relationship_list.html"
 
-class GenericRelationshipEditView(NetBoxEditView):
+class GenericRelationshipEditView(generic.ObjectEditView):
     queryset = models.GenericRelationship.objects.all()
     model_form = forms.RelationshipForm
     template_name = "relationships/generic_relationship_edit.html"
 
-class GenericRelationshipDeleteView(NetBoxDeleteView):
+class GenericRelationshipDeleteView(generic.ObjectDeleteView):
     queryset = models.GenericRelationship.objects.all()
     template_name = "relationships/generic_relationship_delete.html"
