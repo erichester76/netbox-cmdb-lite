a099ee6e4ae6562b34980137a939ec1a70017b19
Eric Hester <eric.hester@umbrella.tech>
2024-12-23 18:35:15 -0500

    .

--- netbox_cmdb_lite/api/serializers.py
+++ netbox_cmdb_lite/api/serializers.py
@@ -1,22 +1,22 @@
 from netbox.api.serializers import NetBoxModelSerializer
-from ..models import ObjectType, GenericObject, RelationshipType, GenericRelationship
+from .. import models
 
 class ObjectTypeSerializer(NetBoxModelSerializer):
     class Meta:
-        model = ObjectType
+        model = models.GenericObjectType
         fields = ['id', 'name', 'attributes']
 
 class GenericObjectSerializer(NetBoxModelSerializer):
     class Meta:
-        model = GenericObject
+        model = models.GenericObject
         fields = ['id', 'name', 'object_type', 'metadata']
 
 class RelationshipTypeSerializer(NetBoxModelSerializer):
     class Meta:
-        model = RelationshipType
+        model = models.RelationshipType
         fields = ['id', 'name', 'description']
 
 class GenericRelationshipSerializer(NetBoxModelSerializer):
     class Meta:
-        model = GenericRelationship
+        model = models.GenericRelationship
         fields = ['id', 'source_content_type', 'source_object_id', 'target_content_type', 'target_object_id', 'relationship_type', 'attributes']
