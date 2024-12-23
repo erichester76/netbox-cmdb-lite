e41fbcbd1892a02ed44d3c623a97a448f255c0ff
Eric Hester <eric.hester@umbrella.tech>
2024-12-22 20:33:33 -0500

    added license and fixed references to views

--- netbox_cmdb_lite/navigation.py
+++ netbox_cmdb_lite/navigation.py
@@ -2,22 +2,22 @@ from netbox.plugins import PluginMenu, PluginMenuItem
 
 mgmt_items = [
         PluginMenuItem(
-            link="object_type_list",
+            link="plugins:netbox_cmdb_lite:object_type_list",
             link_text="Object Types",
             permissions=["cmdb_lite.view_objecttype"],
         ),
         PluginMenuItem(
-            link="relationship_type_list",
+            link="plugins:netbox_cmdb_lite:relationship_type_list",
             link_text="Relationship Types",
             permissions=["cmbd_lite.view_relationshiptype"],
         ),
         PluginMenuItem(
-            link="generic_object_list",
+            link="plugins:netbox_cmdb_lite:generic_object_list",
             link_text="Objects",
             permissions=["cmbd_lite.view_genericobject"],
         ),
         PluginMenuItem(
-            link="generic_relationship_list",
+            link="plugins:netbox_cmdb_lite:generic_relationship_list",
             link_text="Relationships",
             permissions=["cmbd_lite.view_genericrelationship"],
         ),
--- setup.py
+++ setup.py
@@ -1,17 +1,20 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='netbox_cmdb_lite',
+    name='netbox-cmdb-lite',
     version='0.1',
     description='A lightweight CMDB plugin for NetBox',
-    url='https://github.com/clemson-netbox/netbox_cmdb_lite',
+    url='https://github.com/clemson-netbox/netbox-cmdb-lite',
     author='Eric Hester',
     author_email='hester1@clemson.edu',
-    license='MIT',
+    license='Apache 2.0',
     packages=find_packages(),
     install_requires=[
         'django_tables2',
     ],
     include_package_data=True,
+    package_data={
+        '': ['LICENSE'],
+    },
     zip_safe=False,
 )
