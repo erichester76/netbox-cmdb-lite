3392f1a4d0c5e8e669bc2b8fa3b460c7a39e85a2
Eric Hester <eric.hester@umbrella.tech>
2024-12-24 18:27:19 -0500

    fixed serializers

--- netbox_cmdb_lite/api/serializers.py
+++ netbox_cmdb_lite/api/serializers.py
@@ -1,22 +1,40 @@
+from rest_framework import serializers
 from netbox.api.serializers import NetBoxModelSerializer
-from .. import models
+from . import models 
+from django.contrib.contenttypes.models import ContentType
 
+# Serializer for GenericObjectType
 class GenericObjectTypeSerializer(NetBoxModelSerializer):
     class Meta:
         model = models.GenericObjectType
-        fields = '__all__'
+        fields = ['id', 'name', 'attributes', 'created', 'last_updated']
 
+# Serializer for GenericObject
 class GenericObjectSerializer(NetBoxModelSerializer):
+    object_type = serializers.PrimaryKeyRelatedField(queryset=models.GenericObjectType.objects.all())
+    metadata = serializers.JSONField()
+
     class Meta:
         model = models.GenericObject
-        fields = '__all__'
+        fields = ['id', 'name', 'object_type', 'metadata', 'created', 'last_updated']
 
+# Serializer for RelationshipType
 class RelationshipTypeSerializer(NetBoxModelSerializer):
     class Meta:
         model = models.RelationshipType
-        fields = '__all__'
+        fields = ['id', 'name', 'description', 'created', 'last_updated']
 
+# Serializer for GenericRelationship
 class GenericRelationshipSerializer(NetBoxModelSerializer):
+    source_content_type = serializers.PrimaryKeyRelatedField(queryset=ContentType.objects.all())
+    source_object_id = serializers.IntegerField()
+    target_content_type = serializers.PrimaryKeyRelatedField(queryset=ContentType.objects.all())
+    target_object_id = serializers.IntegerField()
+
     class Meta:
         model = models.GenericRelationship
-        fields = '__all__'
+        fields = [
+            'id', 'source_content_type', 'source_object_id',
+            'target_content_type', 'target_object_id',
+            'relationship_type', 'attributes', 'created', 'last_updated'
+        ]
