7dc2d52516560d275a00c9e55a6f9c9723a1e1d2
Eric Hester <eric.hester@umbrella.tech>
2024-12-28 12:57:34 -0500

    .

--- netbox_cmdb_lite/forms.py
+++ netbox_cmdb_lite/forms.py
@@ -35,7 +35,6 @@ class GenericObjectForm(forms.ModelForm):
         queryset=models.GenericObjectType.objects.all(),
         label="Object Type",
         required=True,
-        to_field_name="name",
         help_text="Select the object type to load fields dynamically.",
     )
 
